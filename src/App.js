79386aa39d6b8444994455cd117ce8ad48fc7b90
barendan <barendan@gmail.com>
2021-05-10 20:42:45 -0400

    create new map and connect to container on screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useEffect } from "react";
+import mapboxgl from "mapbox-gl";
 import "./App.css";
 
 const App = () => {
+  const BuildMap = () => {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiYmFyZW5kYW4iLCJhIjoiY2tvaG5iZTBhMG81czJwb2FwMDRlYW1pcyJ9.SHXta_yd3s5oPG5hymzdJA";
+
+    // Initialize new map
+    let map = new mapboxgl.Map({
+      container: "map", // container id
+      // style: "mapbox://styles/mapbox/satellite-v9",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-71.057083, 42.361145], // starting position
+      zoom: 12, // starting zoom
+    });
+  };
+
   return (
     <div className="App">
       <h1>ChargeFul</h1>
-      <p>Dummy text goes here. test test</p>
+
+      <div className="map-container">
+        <div id="map"></div>
+        <div className="helper-box">
+          <p>Draw a polygon using the draw tools.</p>
+        </div>
+      </div>
+      <div id="result"></div>
     </div>
   );
 };
